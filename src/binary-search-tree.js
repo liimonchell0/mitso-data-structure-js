9272c903240c739de941c7dd8107bc79129464c2
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2022-12-13 22:08:25 +0300

    выполнено add

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -16,8 +16,26 @@ module.exports = class BinarySearchTree {
   }
 
   add(data) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    const node = new Node(data)
+   if(!this._root){
+    this._root = node;
+    return;
+   }
+    let currentNode = this._root;
+    while(currentNode){
+      if(node.data < currentNode.data){
+        if(!currentNode.left){
+          currentNode = node;
+          return;
+        }
+        currentNode = currentNode.left;
+      } else{
+        if(!currentNode.right){
+          currentNode = currentNode.right;
+          return;
+        }
+      }
+    }
   }
 
   has(/* data */) {
